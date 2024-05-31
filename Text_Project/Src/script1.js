249086b2aed0ca15e266d6a4f514b5a629d6025e
Sonavin <sonavin.rem@gmail.com>
2024-05-31 13:37:51 +0700

    edit_latest version

--- Text_Project/Src/script1.js
+++ Text_Project/Src/script1.js
@@ -4,18 +4,4 @@ function Input_string() {
   document.getElementById("run-text").innerHTML = userInput; // Update content with user input
 }
 
-function t1btn_change() {
-  const runTextElement = document.getElementById("run-text"); // Element where you want the gradient
-  const gradientStyleId = "t1"; // ID containing the gradient style
-
-  // Get the element with the gradient style definition
-  const gradientStyleElement = document.getElementById(gradientStyleId);
-
-  // Check if the element exists (optional)
-  if (gradientStyleElement) {
-    const gradientStyle = gradientStyleElement.style.backgroundImage;
-    runTextElement.style.backgroundImage = gradientStyle;
-  } else {
-    console.error(`Element with ID "${gradientStyleId}" not found.`); // Handle error if element doesn't exist
-  }
-}
+function str_change() {}
